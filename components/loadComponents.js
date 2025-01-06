de64c4c8fd26d5fa98fcdcd5d312fb6ec01f0630
Vintechs-ux <suryabarumalang@gmail.com>
2025-01-06 14:13:29 +0700

    Update loadComponents.js

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -12,5 +12,5 @@ async function loadComponent(elementId, componentPath) {
 
 // Memuat semua komponen saat halaman dimuat
 document.addEventListener('DOMContentLoaded', () => {
-    loadComponent('footer-component', './components/footer.html');
+    loadComponent('footer-component', '/vintechs-ux.github.io/Portofolio/components/footer.html');
 }); 
