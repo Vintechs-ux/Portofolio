bf8407a1fb275d6532d92f96881878860b2a8ad8
Vintechs-ux <suryabarumalang@gmail.com>
2025-01-06 13:52:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -65,13 +65,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 // Hamburger Menu
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
-});
 
 // Menutup menu saat link diklik
 document.querySelectorAll('.nav-links a').forEach(link => {
